672c966cb9ff036d3000c6b05b8b0b01498b7492
JLab <jessesoldat@gmail.com>
2018-09-30 14:24:20 -0500

    set up form for matching users

--- src/app/shared/components/list-card/list-card.component.ts
+++ src/app/shared/components/list-card/list-card.component.ts
@@ -1,4 +1,10 @@
 import { Component, Input, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators
+} from "@angular/forms";
 // Models
 import { Story } from "../../../models/story.model";
 // Data
@@ -11,11 +17,26 @@ const distances = "5,10,15,20,50,100,300,500,1000,3000,5000,10000,100000";
 export class ListCardComponent implements OnInit {
   @Input()
   data: Story;
-  distanceType = "miles";
+  matchUsersForm: FormGroup;
   distances: string[];
-  constructor() {}
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
     this.distances = distances.split(",");
+
+    this.matchUsersForm = this.formBuilder.group({
+      distances: new FormControl(null, [Validators.required]),
+      distanceType: new FormControl("miles", [Validators.required])
+    });
+
+    this.matchUsersForm.controls["distances"].setValue(this.distances[0], {
+      onlySelf: true
+    });
+  }
+
+  ngOnInit() {}
+
+  handleSubmit() {
+    const form = this.matchUsersForm.value;
+    console.log(form);
   }
 }
